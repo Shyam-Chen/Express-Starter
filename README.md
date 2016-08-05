aaca568626dfed8b716f18d5584a79ec6d2a1474
Shyam Chen <shyamchen1994@gmail.com>
2016-08-05 21:53:41 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Express4TS Quick Start
+
+### Getting Started
+##### 1. Download ZIP
+:point_right: [Download](https://github.com/Shyam-Chen/Express4TS-Quick-Start/archive/master.zip) :point_left:
+
+##### 2. Install the Dependency
+```bash
+$ npm install
+```
+
+##### 3. Run the Application
+```bash
+$ npm start
+```
---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req: any, res: any) => {
+  res.send(`
+    <a href="/">Express</a>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <p>Hello Express</p>
+  `);
+});
+
+app.get('/home', (req: any, res: any) => {
+  res.send(`
+    <a href="/">Express</a>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <p>Home Page</p>
+  `);
+});
+
+app.get('/about', (req: any, res: any) => {
+  res.send(`
+    <a href="/">Express</a>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <p>About Page</p>
+  `);
+});
+
+app.use((req: any, res: any) => {
+  res.status(404);
+  res.send('Not found!');
+});
+
+const server = app.listen(3000, 'localhost', () => {
+  const { address, port } = server.address();
+  console.log(`Listening on http://localhost:${port}`);
+});
