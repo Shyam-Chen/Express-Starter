286c9a074690d95a0eb45ebd3a444beab646870a
Shyam Chen <shyamchen1994@gmail.com>
2016-09-07 15:00:49 +0800

    42nd Commit

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,28 +2,17 @@ import * as gulp from 'gulp';
 import * as nodemon from 'gulp-nodemon';
 import * as browserSync from 'browser-sync';
 
-gulp.task('default', ['browser-sync'], () => { });
-
-gulp.task('browser-sync', ['nodemon'], () => {
-  let config: any = {
-    proxy: 'http://localhost:3001',
-    port: 3000,
-  };
-	browserSync(config);
-});
-
-gulp.task('nodemon', (done: any) => {
-	let started: boolean = false;
-	return nodemon({
-		script: 'src/app.js'
-	})
-  .on('start', () => {
-		if (!started) {
-			started = true;
-      done();
-		}
-	})
-  .on('restart', () => {
-    setTimeout(() => browserSync.reload(), 1000);
+gulp.task('nodemon', () => {
+  nodemon({
+    script: 'src/app.js'
   });
 });
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    proxy: 'http://localhost:3001',
+    port: 3000,
+  });
+});
+
+gulp.task('default', ['nodemon', 'browser-sync']);
