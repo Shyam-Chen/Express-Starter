23ed565793a1643a442711acb3418a3fdc62e98a
Shyam Chen <shyamchen1994@gmail.com>
2016-09-19 16:17:57 +0800

    52nd Commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { join } from 'path';
 
 import * as express from 'express';
-const mongoose = require('mongoose');
 
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
@@ -10,37 +9,57 @@ import * as errorhandler from 'errorhandler';
 
 import { route } from './routes';
 
-const app: express.Express = express();
+class Server {
+  public app: express.Express;
+
+  public static bootstrap(): Server {
+    return new Server();
+  }
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    this.app.set('port', (process.env.PORT || 3001));
+    this.app.set('views', join(__dirname, 'views'));
+    this.app.set('view engine', 'pug');
+
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.app.use(cookieParser());
+
+    this.app.use(logger('dev'));
+
+    this.app.use(require('stylus').middleware(join(__dirname, 'public')));
+    this.app.use(express.static(join(__dirname, 'public')));
+
+    this.app.use(route);
+
+    this.app.use(errorhandler());
+
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      interface Error { status?: number; }
+      const err: Error = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+
+    this.app.listen(this.app.get('port'), () => {
+      console.log('App is running on port', this.app.get('port'));
+    });
+  }
+}
+
+/*
+const mongoose = require('mongoose');
+
 const db = mongoose.connection;
 
 mongoose.connect('mongodb://localhost/test');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Connection Succeeded.'));
+*/
 
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-app.use(logger('dev'));
-
-app.use(require('stylus').middleware(join(__dirname, 'public')));
-app.use(express.static(join(__dirname, 'public')));
-
-app.use(route);
-
-app.use(errorhandler());
-
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  interface Error { status?: number; }
-  const err: Error = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-const server = app.listen(3001, 'localhost', () => {
-  const { address, port } = server.address();
-  console.log(`Listening on http://localhost:${port}`);
-});
+const server = Server.bootstrap();
+export = server.app;
