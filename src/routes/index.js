635c1dd182120070b8ca20545b5bdfcccc1a6102
Shyam Chen <shyamchen1994@gmail.com>
2017-02-13 21:38:11 +0800

    143rd Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
   res.send('Backend Starter Kit');
 });
 
+router.get('/api/:coll', (req, res) => {
+  res.send();
+});
+
+router.post('/api/:coll', (req, res) => {
+  res.send();
+});
+
+router.put('/api/:coll', (req, res) => {
+  res.send();
+});
+
+router.delete('/api/:coll', (req, res) => {
+  res.send();
+});
+
 export const route = router;
