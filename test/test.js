1e9a92679f7f117ad07313ef20a79efeb2f79fd7
Shyam Chen <shyamchen1994@gmail.com>
2017-02-13 21:27:48 +0800

    142nd Commit

--- test/test.js
+++ test/test.js
@@ -3,3 +3,9 @@ import { expect } from 'chai';
 describe('test', () => {
   it('true is true', () => expect(true).to.equal(true));
 });
+
+// import { spy } from 'sinon';
+
+// spy();
+// spy(func);
+// spy(object, 'method');
