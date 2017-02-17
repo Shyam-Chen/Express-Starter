0d70d5c152ef992bc578a78efeacd5dbb19333d7
Shyam Chen <shyamchen1994@gmail.com>
2017-02-17 22:21:53 +0800

    148th Commit

--- src/models/index.js
+++ src/models/index.js
@@ -2,11 +2,11 @@ import mongoose, { Schema } from 'mongoose';
 
 const userSchema = new Schema({
   name: String,
-  user_name: { type : String, required : true, unique : true },
-  password: { type : String, required : true },
+  user_name: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   image: String,
-  created_at: { type : Date, default : Date.now },
-  updated_at: { type : Date, default : Date.now }
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
 });
 
 export const User = mongoose.model('user', userSchema);
