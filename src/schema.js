bc1f47971768f107f9d41a6914ffed5829ca8b74
Shyam Chen <shyamchen1994@gmail.com>
2017-04-27 20:07:26 +0800

    209th Commit

--- src/schema.js
+++ src/schema.js
@@ -14,3 +14,20 @@ export default new GraphQLSchema({
     }
   })
 });
+
+
+// import { GraphQLSchema, GraphQLObjectType } from 'graphql';
+
+// import mutations from './mutations';
+// import queries from './queries';
+
+// export default new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: 'Query',
+//     fields: queries
+//   }),
+//   mutation: new GraphQLObjectType({
+//     name: 'Mutation',
+//     fields: mutations
+//   })
+// });
