ed33526af14eec1df869d23a31512e0a0691c104
Shyam Chen <shyamchen1994@gmail.com>
2017-04-30 17:24:23 +0800

    214th Commit

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ENV HOME /Backend-Starter-Kit
 ENV NODE 7
 ENV DEBIAN_FRONTEND noninteractive
 ENV PATH $HOME/.yarn/bin:$PATH
+ENV HEROKU_API_KEY ${HEROKU_TOKEN}
 
 WORKDIR ${HOME}
 ADD . $HOME
@@ -12,6 +13,11 @@ ADD . $HOME
 RUN curl -sL https://deb.nodesource.com/setup_$NODE.x | bash - && \
     curl -o- -L https://yarnpkg.com/install.sh | bash && \
     apt-get update && apt-get install -y nodejs
+
+# heroku
+RUN echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list && \
+    wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add - && \
+    apt-get update && apt-get install -y heroku-toolbelt
 
 # cleanup
 RUN rm -rf /var/lib/apt/lists/*
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,17 @@
 set -eux
 
 if [[ "$MODE" == "docker" ]]; then
-  docker login --email=shyamchen1994@gmail.com --username=Shyam-Chen --password=${HEROKU_TOKEN} registry.heroku.com
-  docker push registry.github.com/Shyam-Chen/Backend-Starter-Kit:latest
+  # docker login --username=Shyam-Chen --password=${HEROKU_TOKEN} registry.heroku.com
+  # export HEROKU_API_KEY=${HEROKU_TOKEN}
+  gem install dpl
+  dpl --provider=heroku --app=backend-go-demo --api-key=${HEROKU_TOKEN} --strategy=git
 fi
+
+# echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+# wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
+# apt-get update
+# apt-get install -y heroku-toolbelt
+# export HEROKU_API_KEY=${HEROKU_TOKEN}
+# heroku login
+# heroku git:remote -a backend-go-demo
+# git push heroku master
