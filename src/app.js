3c605342221e20593bc0a3759c6dd4c62efb0a83
Shyam Chen <shyamchen1994@gmail.com>
2017-05-07 20:30:27 +0800

    223rd Commit

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ import express from 'express';
 import jwt from 'express-jwt';
 import graphql from 'express-graphql';
 import mongoose from 'mongoose';
+import history from 'express-history-api-fallback';
 import compression from 'compression';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
-import { routes, listRoutes } from './routes';
+import { listRoutes } from './routes';
 import schema from './graphql';
 
 const app = express();
+const root = join(__dirname, '..', 'public');
 
 app.set('port', (process.env.PORT || 8000));
 app.set('mongodb-uri', (process.env.MONGODB_URI || 'mongodb://backend-go:backend-go@ds113871.mlab.com:13871/backend-go-demo'));
@@ -21,7 +23,8 @@ mongoose.connect(app.get('mongodb-uri'));
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', () => console.log('Connection Succeeded.'));
 
-app.use(express.static(join(__dirname, '..', 'public')));
+app.use(express.static(root));
+app.use(history('index.html', { root }));
 app.use(compression());
 app.use(cors());
 app.use(morgan('tiny'));
@@ -41,7 +44,6 @@ app.use('/graphql', graphql(req => ({
   }
 })));
 
-app.use('/', routes);
 app.use('/list', listRoutes);
 
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,1 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', (req, res) => {
-  res.render('index.html');
-});
-
-// router.post('/auth', (req, res, next) => {
-//
-// });
-
-export const routes = router;
 export * from './list';
