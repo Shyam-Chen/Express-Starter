fe443cade9ec758f7072e65d210a9b6522b9eed4
Shyam Chen <shyamchen1994@gmail.com>
2017-09-06 14:49:36 +0800

    314th Commit

--- test/api.spec.js
+++ test/api.spec.js
@@ -4,25 +4,17 @@ import { graphql } from 'graphql';
 import { server } from '~/app';
 import { schema } from '~/graphql';
 
-describe('API', () => {
-  it('responds to /__/list', async () => {
-    const { statusCode, body } = await request(server).get('/__/list');
-    expect(statusCode).toBe(200);
+describe('REST', () => {
+  it('list', async () => {
+    const { body } = await request(server).get('/__/list');
     expect(body).toBeDefined();
   });
+});
 
-  it('responds to /__/list', async () => {
-    const query = `
-      {
-        list {
-          _id
-          text
-        }
-      }
-    `;
-
+describe('GraphQL', () => {
+  it('list', async () => {
+    const query = `{ list { _id text } }`;
     const { data: { list } } = await graphql(schema, query);
-
     expect(list).toBeDefined();
   });
 });
