f93a48af67a26e2912148d9e8a158cb34099189d
Shyam Chen <shyamchen1994@gmail.com>
2017-09-06 14:46:24 +0800

    313th Commit

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,11 +1,28 @@
 import request from 'supertest';
+import { graphql } from 'graphql';
 
 import { server } from '~/app';
+import { schema } from '~/graphql';
 
 describe('API', () => {
   it('responds to /__/list', async () => {
     const { statusCode, body } = await request(server).get('/__/list');
     expect(statusCode).toBe(200);
     expect(body).toBeDefined();
+  });
+
+  it('responds to /__/list', async () => {
+    const query = `
+      {
+        list {
+          _id
+          text
+        }
+      }
+    `;
+
+    const { data: { list } } = await graphql(schema, query);
+
+    expect(list).toBeDefined();
   });
 });
