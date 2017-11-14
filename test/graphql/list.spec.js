d7d3b415ab702380523e932c2a0518d471dfc493
Shyam-Chen <shyamchen1994@gmail.com>
2017-11-14 14:47:12 +0800

    394th Commit

--- test/graphql/list.spec.js
+++ test/graphql/list.spec.js
@@ -3,7 +3,7 @@ import { mockServer } from 'graphql-tools';
 import schema from '~/graphql';
 
 describe('GraphQL', () => {
-  it('list', async () => {
+  it('should get a text list', async () => {
     const query = `{ list { _id text } }`;
     const server = mockServer(schema);
     const { data: { list } } = await server.query(query);
--- test/rest/list.spec.js
+++ test/rest/list.spec.js
@@ -7,8 +7,52 @@ describe('REST', () => {
     server.close();
   });
 
-  it('list', async () => {
-    const { statusCode } = await request(server).get('/__/list');
+  it('should get a text list', async () => {
+    const { statusCode } = await request(server)
+      .get('/__/list');
+
+    expect(statusCode).toBe(200);
+  });
+
+  it('should get a list length', async () => {
+    const { statusCode } = await request(server)
+      .get('/__/list/count');
+
+    expect(statusCode).toBe(200);
+  });
+
+  it('should get a item from ID in list', async () => {
+    const { statusCode } = await request(server)
+      .get('/__/list?_id=59901c7dbc9187001ec32c7b');
+
+    expect(statusCode).toBe(200);
+  });
+
+  it('should search a text in list', async () => {
+    const { statusCode } = await request(server)
+      .get('/__/list?text=v');
+
+    expect(statusCode).toBe(200);
+  });
+
+  it('should /__/list/pagination/2/5', async () => {
+    const { statusCode } = await request(server)
+      .get('/__/list/pagination/2/5');
+
+    expect(statusCode).toBe(200);
+  });
+
+  it('should create a item', async () => {
+    const { statusCode } = await request(server)
+      .post('/__/list', { text: 'Web Go' });
+
+    expect(statusCode).toBe(200);
+  });
+
+  it('should /__/list/relational', async () => {
+    const { statusCode } = await request(server)
+      .get('/__/list/relational');
+
     expect(statusCode).toBe(200);
   });
 });
