11c535fd7c74215f25c5554d22ee81a767a1d01b
Shyam Chen <shyamchen1994@gmail.com>
2017-11-26 15:21:57 +0800

    409th Commit

--- src/rest/file-upload.js
+++ src/rest/file-upload.js
@@ -1,5 +1,19 @@
+import { join } from 'path';
 import { Router } from 'express';
+import formidable from 'formidable';
 
 const router = Router();
+
+router.post('/', (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req)
+    .on('fileBegin', (name, file) => {
+      file.path = join(__dirname, 'uploads', file.name);
+    })
+    .on('file', (name, file) => {
+      res.json({ message: `${file.name} Uploaded` });
+    });
+});
 
 export default router;
--- src/rest/index.js
+++ src/rest/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import textListRoutes from './text-list';
 import authorizationRoutes from './authorization';
 import emailRoutes from './email';
+import fileUploadRoutes from './file-upload';
 
 const router = express.Router();
 
 router.use('/list', textListRoutes);
 router.use('/authorization', authorizationRoutes);
 router.use('/email', emailRoutes);
+router.use('/file-upload', fileUploadRoutes);
 
 export default router;
