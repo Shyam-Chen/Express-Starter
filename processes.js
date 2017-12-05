e384081cc4c2bbac8b7cb8640fc1173f20f61d59
Shyam-Chen <shyamchen1994@gmail.com>
2017-12-05 17:02:59 +0800

    418th Commit

--- processes.js
+++ processes.js
@@ -1,12 +1,10 @@
-// @flow
-
-import pm2 from 'pm2';
+const pm2 = require('pm2');
 
 pm2.connect(() => {
   pm2.start(
     {
       name: 'Backend-Starter-Kit',
-      script: `${__dirname}/api.js`,
+      script: `${__dirname}/dist/api.js`,
       max_memory_restart: `${process.env.WEB_MEMORY || 512}M`,
       exec_mode: 'cluster',
       instances: process.env.WEB_CONCURRENCY || -1
@@ -15,7 +13,7 @@ pm2.connect(() => {
       if (err) return console.error(`Error while launching applications ${err.stack || err}.`);
       console.log('PM2 and application has been succesfully started.');
 
-      pm2.launchBus((busErr, bus) => {
+      pm2.launchBus((err, bus) => {
         console.log('PM2: Log streaming started.');
         bus.on('log:out', packet => console.log(`App (out): ${packet.process.name} - ${packet.data}`));
         bus.on('log:err', packet => console.error(`App (err): ${packet.process.name} - ${packet.data}`));
