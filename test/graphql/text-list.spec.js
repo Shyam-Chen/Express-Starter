c904549b29ffd49f8eb64d4dde1ab09bcb8572cb
Shyam-Chen <shyamchen1994@gmail.com>
2017-12-15 18:35:09 +0800

    429th Commit

--- test/graphql/text-list.spec.js
+++ test/graphql/text-list.spec.js
@@ -15,4 +15,14 @@ describe('GraphQL', () => {
     expect(statusCode).toBe(200);
     expect(list).toBeDefined();
   });
+
+  it('nice _id', async () => {
+    const _id = '5a2653700b81820041010afe';
+    const { statusCode, body: { data: { list } } } = await request(server)
+      .post('/__/graphql')
+      .send({ query: `query { list(_id: "${_id}") { _id text } }` });
+
+    expect(statusCode).toBe(200);
+    expect(list).toBeDefined();
+  });
 });
