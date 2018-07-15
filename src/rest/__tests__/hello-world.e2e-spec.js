20b03913c20f33b2d52e92001442a2969650ccca
Shyam Chen <shyamchen1994@gmail.com>
2018-07-15 13:31:09 +0800

    489th Commit

---
+++ src/rest/__tests__/hello-world.e2e-spec.js
@@ -0,0 +1,17 @@
+import request from 'supertest';
+
+import api from '~/api';
+
+describe('Hello World', () => {
+  afterEach(async () => {
+    await api.close();
+  });
+
+  it('should get a hello world', async () => {
+    const { statusCode, text } = await request(api)
+      .get('/api/hello-world');
+
+    expect(statusCode).toBe(200);
+    expect(text).toBe('Hello, World!');
+  });
+});
