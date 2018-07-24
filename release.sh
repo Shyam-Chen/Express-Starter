f6f7c9bd3341fc7bb9705d1e969755d25fad9f38
Shyam Chen <shyamchen1994@gmail.com>
2018-07-24 20:25:04 +0800

    491st Commit

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+echo "Q1. ENV?"
+read ENV
+
+echo "Q2. IMAGE_NAME?"
+read IMAGE_NAME
+
+echo "Q3. IMAGE_TAG?"
+read IMAGE_TAG
+
+echo "Q4. DOCKER_ID_USER?"
+read DOCKER_ID_USER
+
+docker build -f tools/Dockerfile.$ENV -t $IMAGE_NAME:$IMAGE_TAG .
+
+docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG
+docker push $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG
