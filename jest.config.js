e609294976cfcab36c6189fe5c195d5fbe208be1
Shyam Chen <shyamchen1994@gmail.com>
2018-11-06 14:25:53 +0800

    515th Commit

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/tools/',
+  ],
+  moduleNameMapper: {
+    '~(.*)': '<rootDir>/src$1',
+  },
   setupTestFrameworkScriptFile: '<rootDir>/tools/setup-test.js',
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    process.env.JEST_ENV === 'e2e' ? '.*\\.spec.ts$' : '.*\\.e2e-spec.ts$',
+    process.env.JEST_ENV === 'e2e' ? '.*\\.spec.js$' : '.*\\.e2e-spec.js$',
   ],
   testURL: 'http://localhost/',
   transform: {
