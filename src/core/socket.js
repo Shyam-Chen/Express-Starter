e54e1a5aceff4ba9db262e2cdc0188348e9a89e4
Shyam Chen <shyamchen1994@gmail.com>
2019-03-13 18:24:46 +0800

    549th Commit

--- src/core/socket.js
+++ src/core/socket.js
@@ -1,14 +1,11 @@
 import socket from 'socket.io';
-import socketRedis from 'socket.io-redis';
 import chalk from 'chalk';
 
-import { REDIS_PORT, REDIS_HOST } from '~/env';
 import server from '~/api';
 
 export const io = socket.listen(server);
 
 io.origins(['*:*']);
-io.adapter(socketRedis({ host: REDIS_HOST, port: REDIS_PORT }));
 
 io.on('connection', (connSocket) => {
   console.log(chalk.hex('#009688')(' [*] Socket: Connection Succeeded.'));
