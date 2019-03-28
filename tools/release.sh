705629e332c8ce70093e6ecbfcab3beb1fc4dadd
Shyam Chen <shyamchen1994@gmail.com>
2019-03-28 10:22:35 +0800

    558th Commit

--- tools/release.sh
+++ tools/release.sh
@@ -4,17 +4,28 @@ set -e
 
 echo "Q1. ENV?"
 read ENV
+echo
 
 echo "Q2. IMAGE_NAME?"
 read IMAGE_NAME
+echo
 
 echo "Q3. IMAGE_TAG?"
 read IMAGE_TAG
+echo
 
 echo "Q4. DOCKER_ID_USER?"
 read DOCKER_ID_USER
+echo
 
-docker build -f ./tools/$ENV.Dockerfile -t $IMAGE_NAME:$IMAGE_TAG .
+read -p "Releasing $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG in $ENV mode - are you sure? (y/n) " -n 1 -r
+echo
 
-docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG
-docker push $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Releasing $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG in $ENV mode ..."
+
+  docker build -f ./tools/$ENV.Dockerfile -t $IMAGE_NAME:$IMAGE_TAG .
+
+  docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG
+  docker push $DOCKER_ID_USER/$IMAGE_NAME:$IMAGE_TAG
+fi
