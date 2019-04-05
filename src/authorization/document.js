a890ec30744643e1e4e0a711ee7cbca8cfb20ea3
Shyam Chen <shyamchen1994@gmail.com>
2019-04-05 11:25:34 +0800

    568th Commit

--- src/authorization/document.js
+++ src/authorization/document.js
@@ -1,5 +1,20 @@
 import mongoose, { Schema } from 'mongoose';
 
+/**
+ * @example
+ * {
+ *   username: 'shyam-chen',
+ *   password: '3345678',
+ *   email: 'shyam.chen@gmail.com',
+ *   role: 'user',
+ *   permissions: [
+ *     {
+ *       route: '/blog',
+ *       operations: ['create', 'read', 'update', 'delete']
+ *     }
+ *   ]
+ * }
+ */
 const userSchema = new Schema({
   username: {
     type: String,
@@ -12,6 +27,12 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
+    validate: {
+      validator(value) {
+        return /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i.test(value);
+      },
+      message: ({ value }) => `${value} is not a valid email format`,
+    },
     required: true,
   },
   role: String,
