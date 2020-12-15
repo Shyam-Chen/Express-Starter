15da0d3db262500970f8277d0c2c13574d48f62e
Shyam-Chen <shyamchen1994@gmail.com>
2020-12-15 11:50:00 +0800

    644th Commit

--- src/env.js
+++ src/env.js
@@ -6,9 +6,15 @@ export const PORT = process.env.PORT || 3000;
 
 export const SECRET = process.env.SECRET || 'jbmpHPLoaV8N0nEpuLxlpT95FYakMPiu';
 
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://web-go-user:web-go-user@ds133961.mlab.com:33961/web-go-demo';
-export const POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://ymuxoegt:ONfBcCQylth3boOdUE2EkcZbC2OAbtcm@tantor.db.elephantsql.com:5432/ymuxoegt';
-export const REDIS_URL = process.env.REDIS_URL || 'redis://:8UhZYvM76U8P93BG76DO8zYNmd8KZ6Z5@redis-12468.c124.us-central1-1.gce.cloud.redislabs.com:12468/4';
+export const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  'mongodb://web-go-user:web-go-user@ds133961.mlab.com:33961/web-go-demo';
+export const POSTGRES_URL =
+  process.env.POSTGRES_URL ||
+  'postgres://ymuxoegt:ONfBcCQylth3boOdUE2EkcZbC2OAbtcm@tantor.db.elephantsql.com:5432/ymuxoegt';
+export const REDIS_URL =
+  process.env.REDIS_URL ||
+  'redis://:8UhZYvM76U8P93BG76DO8zYNmd8KZ6Z5@redis-12468.c124.us-central1-1.gce.cloud.redislabs.com:12468/4';
 
 export const CLOUDINARY_CONFIG = {
   cloud_name: process.env.CLOUDINARY_NAME || 'sample',
@@ -47,4 +53,6 @@ export const AUTH = {
 
 export const RATE_LIMIT = process.env.RATE_LIMIT || 0;
 
-export const SENTRY_DSN = process.env.SENTRY_DSN || 'https://70484e0dda784a1081081ca9c8237792:51b5a95ee1e545efba3aba9103c6193e@sentry.io/236866';
+export const SENTRY_DSN =
+  process.env.SENTRY_DSN ||
+  'https://70484e0dda784a1081081ca9c8237792:51b5a95ee1e545efba3aba9103c6193e@sentry.io/236866';
